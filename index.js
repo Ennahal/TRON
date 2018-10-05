c997d84ea46f600d3987852fb3571bcfdc515f80
Florian Richy <florian.richy@gmail.com>
2018-10-05 11:45:36 +0200

    back

--- index.js
+++ index.js
@@ -1,129 +1,111 @@
+// Import de la classe
+// const Game = require('models/Game.class.js');
+// Instanciation de l'objet
+// var game = new Game();
+/*
+client => server
+move_left
+move_right
+move_up
+move_down
+use_powerup
+game_leave
+game_ready
+game_join(id_game)
+game_create(type)// ffa ou ftf
+user_register({login:"", avatar:""})
+message_ffa(content)
+message_ftf(content)
+
+server => client
+game_list([{id:"", players:""}])
+player_list([{login:"", avatar:""}])
+player_dead
+game_start
+message_ffa({login:"", avatar:"", content:""})
+message_ftf({login:"", avatar:"", content:""})
+*/
+/**
+index.js => Server => io.connection, socket.user_register, socket.game_create, socket.message => le lobby par défaut
+Game.class.js => Game => socket.game_join, socket.game_leave, socket.game_ready
+// uniqid pour la Game => socket.game_join_ID
+	=> Map.class.js => /!\ [80][60] => map.collide(x, y);
+Player.class.js => Player => haut / bas / gauche / droite : droite / gauche + vitesse up/vitesse down : espace
+**/
+
+//--------------------------------- ancien index.js
+const uniqid = require('uniqid');// npm install --save uniqid
 const PORT = 3000;
-const HISTORY_MAX_MESSAGE = 50;
-const SPAM_MAX_MESSAGE = 2;
 class Server {
     constructor() {
         this.express = require('express');
         this.app = this.express();
         this.http = require('http').Server(this.app);
         this.io = require('socket.io')(this.http);
-        this.userList = [];// Liste des utilisateurs connnectés
-        // this.msgList = [];// Liste des anciens messages -> has been, vu qu'on a l'historique indépendamment pour chaque canal
-        // key : nom du canal, value : liste des messages (historique)
-        this.chanList = { "home": [], "gaming": [] };// Liste des canaux/rooms
+        this.players = {};
+        this.games = {};
         this.init();
+    }
+    createGame(player) {
+        var game = new Game(player);
+        this.games[game.id] = game;
+        // uniqid(); => dans Game.constructor
+        // Faire des trucs cools genre new Game :p
+    }
+    removePlayer(player) {
+        delete this.players[player.id];
+    }
+    joinGame(player, gameId) {
+        this.games[gameId].join(player);
+    }
+    createPlayer(socket, info) {// const Player = require('models/Player.class.js');
+        var player = new Player(socket, user);
+        this.players[player.id] = player;
+        console.log("New Player > ", player.login);
+        // Se déconnecter
+        socket.on("disconnect", this.removePlayer.bind(this, player));
+        // Créer une partie
+        socket.on("game_create", this.createGame.bind(this, player));
+        // Envoyer la liste des games
+        socket.emit("game_list", Object.values(this.games));
+        // Envoyer la liste des joueurs
+        socket.emit("player_list", Object.values(this.players));
+        // Rejoindre une partie par son id
+        socket.on("game_join", this.joinGame.bind(this, player));
     }
     init() {
         this.app.use(this.express.static('public'));
-        // 1. la socket se connecte
         this.io.on('connection', (socket) => {
-            // 2. l'utilisateur envoie le formulaire de register (login + avatar)
-            // this.socket.emit("user_register", {login:login, avatar:avatar});
-            socket.on('user_register', (user) => {
-                // Par défaut le nouvel utilisateur rejoint le canal de base "home"
-                socket.join("home");
-                // Vérification taille du login de l'utilisateur (ternaire)
-                user.login = user.login.length > 20 ? user.login.substring(0, 20) : user.login;
-                // Enregistrer l'adresse IP
-                user.ip = socket.handshake.address;
-                // Passer writing à false par défaut
-                user.writing = false;
-                // Faire le lien entre une socket et son propriétaire (on va relier la socket au couple login/avatar)
-                socket.user = user;
-                // On stock le nouvel utilisateur dans notre liste d'users connectés
-                // Avant d'insérer l'utilisateur dans la liste
-                user.position = this.userList.length;
-                this.userList.push(user);
-                // Petit debug
-                console.log("New User > ", user, "Nb users > ", this.userList.length);
-                // 3. envoie de user_connected => Prévenir tous les utilisateurs du nouvel arrivant
-                this.io.emit("user_connected", user, this.userList, { login: "Serveur", content: "Un nouvel utilisateur s'est connecté : " + user.login, avatar: "https://static.thenounproject.com/png/371299-200.png", date: new Date() });
-                // Avant on envoyait directement this.msgList
-                // Maintenant il faut envoyer l'historique du canal en question, du coup à la connexion on envoie l'historique de "home"
-                socket.emit("message_history", "home", this.chanList["home"]);
-                // 4. possibilité de user_disconnected => event en cas de deco
-                socket.on("disconnect", () => {
-                    // On va retirer de notre liste des utilisateurs connectés celui qui s'en va
-                    var ready = false;
-                    var i = 0;
-                    while (i < this.userList.length) {
-                        if (this.userList[i].login == user.login)// login / avatar
-                        {
-                            this.userList.splice(i, 1);
-                            ready = true;
-                        }
-                        if (ready && this.userList[i] !== undefined)
-                            this.userList[i].position--;
-                        i++;
-                    }
-                    // On prévient les utilisateurs restant du départ
-                    this.io.emit("user_disconnected", user, this.userList, { login: "Serveur", content: "Un utilisateur s'est déconnecté : " + user.login, avatar: "https://static.thenounproject.com/png/371299-200.png", date: new Date() });
-                });
-                // 5. possibilité de message => Lors de la réception d'un message
-                // On a rajouté le channel dans lequel le message est écrit
-                socket.on("message", (channel, content = "") => {
-                    // Message vide ou trop long ?
-                    if (content.trim().length > 0 && content.trim().length < 1024)// Vérifier la validité du message
-                    {
-                        // Vérifier et gérer le SPAM
-                        if (!socket.interval) {
-                            socket.nbr = 0;
-                            socket.interval = setTimeout(() => {
-                                socket.nbr = 0;
-                                socket.interval = undefined;
-                            }, 1000);
-                        }
-                        socket.nbr++;
-                        if (socket.nbr < SPAM_MAX_MESSAGE + 1)// Si NON SPAM
-                        {
-                            // On génére le message
-                            let msg = { date: new Date(), content: content, login: socket.user.login, avatar: socket.user.avatar };
-                            // DEBUG
-                            console.log("New Message > ", channel, msg);
-                            // Avant on avait juste la transmission du message à tout le monde via io.emit
-                            // Maintenant on choisit d'envoyer le message seulement au canal auquel il est destiné
-                            this.io.to(channel).emit("message", channel, msg);
-                            //      |<---- On envoie vers "channel"
-                            // On ajoute comme d'habitude le message dans l'historique de ce canal
-                            this.chanList[channel].push(msg);
-                            // On fait le ménage si l'historique est trop grand
-                            if (this.chanList[channel].length > HISTORY_MAX_MESSAGE)
-                                this.chanList[channel].shift();
-                        }
-                        else// Si SPAM on envoie a l'utilisateur son propre message avec un message de service
-                            socket.emit("message", channel, { date: new Date(), content: "Ce message ne peut pas être affiché : " + content, login: "Serveur", avatar: "https://static.thenounproject.com/png/371299-200.png" });
-                    }
-                    this.io.emit("user_resting", socket.user);
-                });
-                // On envoie la liste des canaux (les clefs de this.chanList)
-                socket.emit("channel_history", Object.keys(this.chanList));
-                // L'utilisateur rejoint ou quitte une room
-                socket.on("channel_join", (channel) => {
-                    // DEBUG
-                    console.log(socket.user.login, " join ", channel);
-                    // On vérifie si le canal existe si PAS
-                    if (this.chanList[channel] === undefined) {
-                        // Si il existe pas, on le créé, avec par défaut un historique vide
-                        this.chanList[channel] = [];
-                        // On envoie à tous le fait qu'un nouveau canal existe
-                        this.io.emit("channel_new", channel);
-                    }
-                    // Par défaut quand un utilisateur envoie channel_join c'est quand même pour le rejoindre
-                    socket.join(channel);
-                });
-                socket.on("channel_leave", (channel) => {
-                    socket.leave(channel);
-                });
-                // Utilisateur en train d'écrire ou pas
-                socket.on("user_writing", () => {
-                    socket.user.writing = true;
-                    this.io.emit("user_writing", socket.user);
-                });
-                socket.on("user_resting", () => {
-                    socket.user.writing = false;
-                    this.io.emit("user_resting", socket.user);
-                });
-            })
+            socket.on('user_register', this.createPlayer.bind(this, socket));
+			/* A VOIR AVEC LE FRONT POUR LES MESSAGES
+			socket.on("message", (channel, content = "") =>
+			{
+				if (content.trim().length > 0 && content.trim().length < 1024)
+				{
+					if (!socket.interval)
+					{
+						socket.nbr = 0;
+						socket.interval = setTimeout(() =>
+						{
+							socket.nbr = 0;
+							socket.interval = undefined;
+						}, 1000);
+					}
+					socket.nbr++;
+					if (socket.nbr < SPAM_MAX_MESSAGE + 1)
+					{
+						let msg = {date:new Date(), content:content, login:socket.user.login, avatar:socket.user.avatar};
+						console.log("New Message > ", channel, msg);
+						this.io.to(channel).emit("message", channel, msg);
+						this.chanList[channel].push(msg);
+						if (this.chanList[channel].length > HISTORY_MAX_MESSAGE)
+							this.chanList[channel].shift();
+					}
+					else
+						socket.emit("message", channel, {date:new Date(), content:"Ce message ne peut pas être affiché : "+content, login:"Serveur", avatar:"https://static.thenounproject.com/png/371299-200.png"});
+				}
+				this.io.emit("user_resting", socket.user);
+			});*/
         });
     }
     listen(port) {
@@ -132,5 +114,5 @@ class Server {
         });
     }
 }
-var server = new Server();
+var server = new Server()
 server.listen(PORT);
---
+++ models/Game.class.js
@@ -0,0 +1,77 @@
+//Game.class.js => Game => socket.game_join, socket.game_leave, socket.game_ready
+// uniqid pour la Game => socket.game_join_ID
+// creation de l'unique Id
+const NB_PLAYER = 4;
+const HEIGHT = 80;
+const WIDTH = 60;
+const uniqid = require('uniqid');
+const uniqid = require('uniqid');
+class Game {
+    constructor(player) {
+        this.host = player;
+        this.listPlayer = [];
+        this.listPlayerReady = [];
+        this.id = uniqid();
+        this.join(player);
+        this.listLooser = [];
+    }
+    go() {
+        // gameloop
+        setInterval(() => {
+            let i = 0;
+            while (i < this.listPlayer.length) {
+                if (this.listPlayer[i].isAlive)
+                    this.listPlayer[i].move(this.map);
+                i++;
+            }
+
+            // listPlayer mort => listLooser 
+            // listLooser == nbPLayer - 1 => emit (bravo)
+            // Dire a tout que la partie est finie, finir la partie, envoyer le score/podium, etc...
+        }, 100);
+    }
+    join(player) {
+        player.socket.on('game_leave', () => {
+            let i = 0
+            while (i < this.listPlayer.length) {
+                if (this.listPlayer[i].id == player.id)
+                    this.listPlayer.splice(i, 1);
+                i++;
+            }
+            i = 0
+            while (i < this.listPlayerReady.length) {
+                if (this.listPlayerReady[i].id == player.id)
+                    this.listPlayerReady.splice(i, 1);
+                i++;
+            }
+        });
+        if (this.listPlayer.length < NB_PLAYER) {
+            this.listPlayer.push(player);
+            player.socket.once('game_ready', () => {
+                this.listPlayerReady.push(player);
+                this.gameReady();
+            });// game_ready, game_start...
+        }
+    }
+    sendAll(type, content) {
+		/* let i = 0;
+        while (i < this.listPlayerReady.length) {
+            this.listPlayerReady[i].socket.emit(type, content);
+            i++;
+      } */
+        this.listPlayerReady.map(i => i.socket.emit(type, content))
+    }
+    gameReady() {
+        if (this.listPlayerReady.length == NB_PLAYER) {
+            // générer la map
+            this.map = new Map(HEIGHT, WIDTH, this.listPlayerReady);
+            this.sendAll("game_start");
+            setTimeout(() => {
+                this.go();
+            }, 5000);
+            return true;
+        }
+        return false;
+    }
+}
+module.export = Game;
